823db445eab023ff65df754834813649a1fcb4af
ekno27 <edgar.canozelaya.6@my.csun.edu>
2018-07-12 20:12:07 -0700

    added front end notes

--- public/app.js
+++ public/app.js
@@ -1,11 +1,44 @@
 console.log("hello");
 
 var headline="none";
+var _id = "none"
 $(".headline").on("click", function(){
+    _id = $(this).attr("db_id")
+    console.log(_id);
     headline = $(this).text();
     $("#headline").text(headline);
-
     $("#results-modal").modal("toggle");
+    //making a call for the article
+   
+    $.getJSON(`/articles/${_id}`, function(data){
+        populateNoteSpace(data);
+
+    });
 });
 
+$("#modalSubmit").on("click", function(){
 
+ var note = $("#note").val().trim();
+ console.log(note);
+ var submitNote = {
+     note: note
+ }
+ //sending note to backend 
+ $.post(`/articles/${_id}`,submitNote,function(){
+    console.log("Note Added!");
+    // location.reload(true);
+ });
+});
+
+// $.getJSON("article/")
+
+
+function populateNoteSpace(item){
+    console.log(item);
+    if(item.note===undefined){
+        $("#note-space").append(`<p>No notes so far!</p>`)
+    }else{
+    $("#note-space").append(`<p>${item.note.note}</p>`)
+
+    }
+}
--- server.js
+++ server.js
@@ -129,6 +129,32 @@ app.get("/scrape", function(req,res){
     res.redirect("/");
 });
 
+//route for grabbing
+app.get("/articles/:id",function(req,res){
+    db.MusicArticle.findOne({_id:req.params.id})
+    .populate("note")
+    .then(function(dbArticle){
+        res.json(dbArticle);
+    }).catch(function(err){
+        res.json(err);
+    });
+});
+
+
+//add the note to its corresponding article
+app.post("/articles/:id",function(req,res){
+    console.log(req.body);
+    db.Note.create(req.body)
+    .then(function(dbNote){
+        return db.MusicArticle.findOneAndUpdate({_id:req.params.id},{note: dbNote._id }, {new: true});
+    }).then(function(dbArticle){
+        res.json(dbArticle);
+    }).catch(function(err){
+        console.log(err);
+        res.json(err);
+    });
+});
+
 // listen
 app.listen(PORT, function(){
     console.log("App running on port " +  PORT);
